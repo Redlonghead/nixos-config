c945faae396930fb15efabd1804473fe93ecc7e3
Redlonghead <git@beardit.net>
2024-07-16 15:42:56 -0500

    feat: remove syncthing module in nixos-secrets

--- hosts/common/optional/services/syncthing.nix
+++ hosts/common/optional/services/syncthing.nix
@@ -1,14 +1,60 @@
-{ inputs, pkgs, config, ... }:
+{ inputs, pkgs, config, configVars, ... }:
 
 {
 
-  #TODO Relized after that I can just leave it in public so refactor back into public repo
-  # And need to generlize it for when I add more hosts.
-  imports = [ inputs.nixos-secrets.nixosModules.syncthing-CB-FW ];
+  sops = {
+    secrets = {
+      # add secrets to be extracted under /run with root priv by default
+      "syncthing/${config.networking.hostName}/key" = {
+        owner = configVars.userSettings.username;
+        group = "syncthing";
+      };
 
-  # Issue #326704 on nixpkg for needing to set
-  # services.syncthing.overrideDevices = false
-  # when declaring devices in nixos config
+      "syncthing/${config.networking.hostName}/cert" = {
+        owner = configVars.userSettings.username;
+        group = "syncthing";
+      };
+
+    };
+
+  };
+
+  services.syncthing = {
+    enable = true;
+    user = configVars.userSettings.username;
+    dataDir = "/home/${configVars.userSettings.username}";
+    configDir = "/home/${configVars.userSettings.username}/.config/syncthing/";
+
+    # FIXME Issue #326704 on nixpkg for needing to set
+    # services.syncthing.overrideDevices = false
+    # when declaring devices in nixos config
+    overrideDevices = false;
+
+    key = config.sops.secrets."syncthing/${config.networking.hostName}/key".path;
+    cert = config.sops.secrets."syncthing/${config.networking.hostName}/cert".path;
+
+    settings = {
+      devices = { "CB-SNAS-01" = { id = "0AM0X7P-J0EACFU-HCJMJER-ZEBN7DK-KE7TMBX-YEZTM4U-Y22JDPP-KIPC3QU"; }; };
+      folders = {
+        "CB-SRC" = {
+          path = "~/src";
+          devices = [ "CB-SNAS-01" ];
+          ignorePerms = false;
+        };
+        "CB-Documents" = {
+          path = "~/Documents";
+          devices = [ "CB-SNAS-01" ];
+        };
+      };
+    };
+  };
+
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true"; # Don't create default ~/Sync folder
+
+  networking.firewall = {
+    allowedTCPPorts = [ 22000 ];
+    allowedUDPPorts = [ 22000 ];
+  };
 
   environment.systemPackages = [
     (pkgs.writeScriptBin "syncthing" "xdg-open https://127.0.0.1:8384")
