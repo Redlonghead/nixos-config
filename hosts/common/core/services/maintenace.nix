111477c6d3388a7ecbb611d75ca67118c1c491d3
Redlonghead <git@beardit.net>
2024-07-16 15:42:56 -0500

    feat: added auto gc and auto store optimise

--- hosts/common/core/services/maintenace.nix
+++ hosts/common/core/services/maintenace.nix
@@ -25,4 +25,15 @@ in
       ''
     );
   };
+
+  nix.gc = {
+    automatic = true;
+    dates = "00:00";
+    options = "--delete-older-than 10d";
+  };
+
+  nix.optimise = {
+    automatic = true;
+    dates = [ "04:00" ];
+  };
 }
