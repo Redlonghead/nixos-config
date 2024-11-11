51fe314b8c4642bff82f7a3bab963c1e5b1270bf
Redlonghead <git@beardit.net>
2024-11-11 13:17:00 -0600

    feat: added some support for dolphin

--- home/beacon/common/optional/ui-apps.nix
+++ home/beacon/common/optional/ui-apps.nix
@@ -9,10 +9,17 @@
     obs-studio
     remmina
     bitwarden
+    
     dolphin
+    kdePackages.qtwayland
+    kdePackages.qtsvg
+    kdePackages.kio-fuse
+    kdePackages.kio-extras
+
     libreoffice-qt
     hunspell # LibraOffice Spellcheck
     hunspellDicts.en_US
+    
     moonlight-qt
     thunderbird
     protonmail-bridge-gui
