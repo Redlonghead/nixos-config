8e860499fc6c4ff60d5c04af836adaf43b78197f
Redlonghead <git@beardit.net>
2024-12-10 00:24:40 -0600

    feat: move flake to 24.11

--- hosts/common/optional/services/pipewire.nix
+++ hosts/common/optional/services/pipewire.nix
@@ -2,8 +2,6 @@
 
 {
   # Enable sound with pipewire.
-  sound.enable = true;
-  sound.mediaKeys.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
