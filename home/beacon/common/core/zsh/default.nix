aefd758b1f74bcae8ed98b0d559a4c26440ba5d6
Redlonghead <git@beardit.net>
2025-06-15 00:00:22 -0500

    refactor: changed oh my posh to use home manager options

--- home/beacon/common/core/zsh/default.nix
+++ home/beacon/common/core/zsh/default.nix
@@ -1,46 +1,49 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
+  programs = {
+    zsh = {
+      enable = true;
 
-  imports = [
-    ./oh-my-posh.nix
-  ];
+      # relative to ~
+      dotDir = ".config/zsh";
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+      autocd = true;
+      autosuggestion.enable = true;
+      history.size = 10000;
+      history.share = true;
 
-  programs.zsh = {
-    enable = true;
+      plugins = [
+        {
+          name = "zsh-term-title";
+          src = "${pkgs.zsh-term-title}/share/zsh/zsh-term-title/";
+        }
+        {
+          name = "cd-gitroot";
+          src = "${pkgs.cd-gitroot}/share/zsh/cd-gitroot";
+        }
+      ];
 
-    # relative to ~
-    dotDir = ".config/zsh";
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
-    autocd = true;
-    autosuggestion.enable = true;
-    history.size = 10000;
-    history.share = true;
+      shellAliases = {
+        cl = "clear";
+        ls = "eza";
+        la = "eza -la";
+        ll = "eza -l";
+        ".." = "cd ..";
+        ncc = "cd ~/src/nixos-config";
+        nsc = "cd ~/src/nixos-secrets";
+        ranger = "yazi";
+        ff = "fastfetch";
+        cat = "bat";
+        ts = "tailscale";
+      };
+    };
 
-    plugins = [
-      {
-        name = "zsh-term-title";
-        src = "${pkgs.zsh-term-title}/share/zsh/zsh-term-title/";
-      }
-      {
-        name = "cd-gitroot";
-        src = "${pkgs.cd-gitroot}/share/zsh/cd-gitroot";
-      }
-    ];
-
-    shellAliases = {
-      cl = "clear";
-      ls = "eza";
-      la = "eza -la";
-      ll = "eza -l";
-      ".." = "cd ..";
-      ncc = "cd ~/src/nixos-config";
-      nsc = "cd ~/src/nixos-secrets";
-      ranger = "yazi";
-      ff = "fastfetch";
-      cat = "bat";
-      ts = "tailscale";
+    oh-my-posh = {
+      enable = true;
+      package = pkgs.oh-my-posh;
+      settings = lib.importJSON ./omp-config.json;
     };
   };
 }
