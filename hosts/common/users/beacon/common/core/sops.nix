199a5c4fc2aecde054b87b47d3e92e00c383dc34
Redlonghead <git@beardit.net>
2025-06-23 10:03:35 -0600

    fix: sops.secrets group value was invalid

--- hosts/common/users/beacon/common/core/sops.nix
+++ hosts/common/users/beacon/common/core/sops.nix
@@ -1,9 +1,12 @@
-{ ... }:
+{
+  config,
+  ...
+}:
 
 {
   sops.secrets."users/beacon/ssh" = {
     owner = "beacon";
-    group = "beacon";
+    inherit (config.users.users.beacon) group;
     path = "/home/beacon/.ssh/id_beacon";
   };
 }
