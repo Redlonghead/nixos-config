eb7325703294afe849e0b08313af512a3a1ac1ef
Redlonghead <git@beardit.net>
2025-07-14 22:23:27 -0500

    refactor/style: reworked stylix.nix
    shortened themePath and useded it, realized stylix was not being used,
    and changed themes to have theme.yaml inside instead of THEME-NAME.yaml

--- home/beacon/common/optional/style/default.nix
+++ home/beacon/common/optional/style/default.nix
@@ -8,62 +8,57 @@
 }:
 
 let
-  themePath =
-    "../../../../../../themes"
-    + ("/" + configVars.userSettings.theme + "/" + configVars.userSettings.theme)
-    + ".yaml";
-  themePolarity = lib.removeSuffix "\n" (
-    builtins.readFile (
-      ./. + "../../../../../../themes" + ("/" + configVars.userSettings.theme) + "/polarity.txt"
-    )
-  );
-  backgroundUrl = builtins.readFile (
-    ./. + "../../../../../../themes" + ("/" + configVars.userSettings.theme) + "/backgroundurl.txt"
-  );
-  backgroundSha256 = builtins.readFile (
-    ./. + "../../../../../../themes/" + ("/" + configVars.userSettings.theme) + "/backgroundsha256.txt"
-  );
+  themePath = (./. + "../../../../../../themes/${configVars.userSettings.theme}");
+  themePolarity = lib.removeSuffix "\n" (builtins.readFile "${themePath}/polarity.txt");
+  backgroundUrl = builtins.readFile "${themePath}/backgroundurl.txt";
+  backgroundSha256 = builtins.readFile "${themePath}/backgroundsha256.txt";
 in
 {
 
   imports = [ inputs.stylix.homeModules.stylix ];
 
-  home.file.".currenttheme".text = configVars.userSettings.theme;
-  stylix.autoEnable = false;
-  stylix.polarity = themePolarity;
-  stylix.image = pkgs.fetchurl {
-    url = backgroundUrl;
-    sha256 = backgroundSha256;
-  };
-  stylix.base16Scheme = ./. + themePath;
+  stylix = {
+    enable = false;
+    autoEnable = false;
+    polarity = themePolarity;
+    image = pkgs.fetchurl {
+      url = backgroundUrl;
+      sha256 = backgroundSha256;
+    };
+    base16Scheme = "${themePath}/theme.yaml";
 
-  stylix.fonts = {
-    monospace = {
-      package = pkgs.nerd-fonts.fira-code;
-    };
-    serif = {
-      package = pkgs.nerd-fonts.fira-code;
-    };
-    sansSerif = {
-      package = pkgs.nerd-fonts.fira-code;
-    };
-    emoji = {
-      name = "Noto Color Emoji";
-      package = pkgs.noto-fonts-emoji-blob-bin;
-    };
-    sizes = {
-      terminal = 18;
-      applications = 12;
-      popups = 12;
-      desktop = 12;
+    # fonts = {
+    #   monospace = {
+    #     package = pkgs.nerd-fonts.fira-code;
+    #   };
+    #   serif = {
+    #     package = pkgs.nerd-fonts.fira-code;
+    #   };
+    #   sansSerif = {
+    #     package = pkgs.nerd-fonts.fira-code;
+    #   };
+    #   emoji = {
+    #     name = "Noto Color Emoji";
+    #     package = pkgs.noto-fonts-emoji-blob-bin;
+    #   };
+    #   sizes = {
+    #     terminal = 18;
+    #     applications = 12;
+    #     popups = 12;
+    #     desktop = 12;
+    #   };
+    # };
+
+    targets = {
+      kde.enable = true;
+      kitty.enable = true;
+      gtk.enable = true;
     };
   };
-
-  stylix.targets.kde.enable = true;
-  stylix.targets.kitty.enable = true;
-  stylix.targets.gtk.enable = true;
 
   home.file = {
+    ".currenttheme".text = configVars.userSettings.theme;
+
     ".config/qt5ct/colors/oomox-current.conf".source = config.lib.stylix.colors {
       template = builtins.readFile ./oomox-current.conf.mustache;
       extension = ".conf";
--- hosts/common/optional/style.nix
+++ hosts/common/optional/style.nix
@@ -7,60 +7,48 @@
 }:
 
 let
-  themePath =
-    "../../../../themes/"
-    + configVars.userSettings.theme
-    + "/"
-    + configVars.userSettings.theme
-    + ".yaml";
-  themePolarity = lib.removeSuffix "\n" (
-    builtins.readFile (
-      ./. + "../../../../themes" + ("/" + configVars.userSettings.theme) + "/polarity.txt"
-    )
-  );
-  myLightDMTheme = if themePolarity == "light" then "Adwaita" else "Adwaita-dark";
-  backgroundUrl = builtins.readFile (
-    ./. + "../../../../themes" + ("/" + configVars.userSettings.theme) + "/backgroundurl.txt"
-  );
-  backgroundSha256 = builtins.readFile (
-    ./. + "../../../../themes/" + ("/" + configVars.userSettings.theme) + "/backgroundsha256.txt"
-  );
+  themePath = (./. + "../../../../themes/${configVars.userSettings.theme}");
+  themePolarity = lib.removeSuffix "\n" (builtins.readFile "${themePath}/polarity.txt");
+  backgroundUrl = builtins.readFile "${themePath}/backgroundurl.txt";
+  backgroundSha256 = builtins.readFile "${themePath}/backgroundsha256.txt";
 in
 {
   imports = [ inputs.stylix.nixosModules.stylix ];
 
-  stylix.autoEnable = true;
-  stylix.polarity = themePolarity;
-  stylix.image = pkgs.fetchurl {
-    url = backgroundUrl;
-    sha256 = backgroundSha256;
-  };
-  stylix.base16Scheme = ./. + themePath;
-  stylix.fonts = {
-    monospace = {
-      package = pkgs.nerd-fonts.fira-code;
+  stylix = {
+    enable = true;
+    autoEnable = false;
+    base16Scheme = "${themePath}/theme.yaml";
+    polarity = themePolarity;
+
+    image = pkgs.fetchurl {
+      url = backgroundUrl;
+      sha256 = backgroundSha256;
     };
-    serif = {
-      package = pkgs.nerd-fonts.fira-code;
-    };
-    sansSerif = {
-      package = pkgs.nerd-fonts.fira-code;
-    };
-    emoji = {
-      name = "Noto Color Emoji";
-      package = pkgs.noto-fonts-emoji-blob-bin;
+
+    # fonts = {
+    #   monospace = {
+    #     package = pkgs.nerd-fonts.fira-code;
+    #   };
+    #   serif = {
+    #     package = pkgs.nerd-fonts.fira-code;
+    #   };
+    #   sansSerif = {
+    #     package = pkgs.nerd-fonts.fira-code;
+    #   };
+    #   emoji = {
+    #     name = "Noto Color Emoji";
+    #     package = pkgs.noto-fonts-emoji-blob-bin;
+    #   };
+    # };
+
+    targets = {
+      lightdm.enable = true;
+      console.enable = true;
     };
   };
 
-  stylix.targets.lightdm.enable = true;
-  services.xserver.displayManager.lightdm = {
-    greeters.slick.enable = true;
-    greeters.slick.theme.name = myLightDMTheme;
-  };
-  stylix.targets.console.enable = true;
-
-  environment.sessionVariables = {
-    QT_QPA_PLATFORMTHEME = "qt5ct";
-  };
-
+  # environment.sessionVariables = {
+  #   QT_QPA_PLATFORMTHEME = "qt5ct";
+  # };
 }
