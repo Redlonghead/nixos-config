fe0deced5ee1d91034532893cdb6980f48b4d4b8
Redlonghead <git@beardit.net>
2025-07-15 22:32:25 -0500

    style/kitty: turn on stylix

--- home/beacon/common/optional/uiApps/kitty.nix
+++ home/beacon/common/optional/uiApps/kitty.nix
@@ -1,19 +1,20 @@
 {
   pkgs,
-  lib,
   ...
 }:
 
 {
-  home.packages = with pkgs; [
-    kitty
-  ];
-  programs.kitty.enable = true;
-  programs.kitty.settings = {
-    background_opacity = lib.mkForce "0.75";
-  };
 
-  programs.zsh.shellAliases = {
-    ksh = "kitten ssh";
+  stylix.targets.kitty.enable = true;
+
+  programs = {
+    kitty = {
+      enable = true;
+      package = pkgs.kitty;
+    };
+
+    zsh.shellAliases = {
+      ksh = "kitten ssh";
+    };
   };
 }
