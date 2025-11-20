cd2967e20a24c15e960d899e872e0a0b05a9b401
Redlonghead <git@beardit.net>
2025-11-19 23:45:35 -0600

    feat: rm ciscoPacketTracer

--- home/beacon/common/optional/school-apps.nix
+++ home/beacon/common/optional/school-apps.nix
@@ -7,7 +7,7 @@
   home.packages = (
     with pkgs;
     [
-      ciscoPacketTracer8
+      # ciscoPacketTracer8 # 8.2.2 can't be downloaded anymore from Cisco
       p3x-onenote
       teams-for-linux
     ]
