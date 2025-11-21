8d609ed7020d89ee62a67c7e298c22564ee2d424
Redlonghead <git@beardit.net>
2025-11-21 00:31:28 -0600

    fix: librepods package - rust
    fixed package to be able to run. rev from kavishdevar/librepods#241

--- pkgs/librepods/default.nix
+++ pkgs/librepods/default.nix
@@ -1,103 +1,70 @@
 {
-  stdenv,
-  lib,
   fetchFromGitHub,
-  rustPlatform,
-  cmake,
-  pkg-config,
-  udev,
-  dbus,
   libpulseaudio,
-  qt6,
+  vulkan-loader,
+  rustPlatform,
+  libxkbcommon,
+  makeWrapper,
+  pkg-config,
+  wayland,
+  libGL,
+  dbus,
+  xorg,
+  lib,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "librepods";
-  version = "14.1.1";
+  version = "0.2.0-linux-rust";
 
   src = fetchFromGitHub {
     owner = "kavishdevar";
     repo = "librepods";
-    rev = "a01e16792a73deb34c5bd0c4aa019c496642ee71";
-    hash = "sha256-ZvHbSSW0rfcsNUORZURe0oBHQbnqmS5XT9ffVMwjIMU=";
+    # tag = "v${finalAttrs.version}";
+    rev = "4737cbfc2c1a4e227e42d095c49ab43bd8d7b64a";
+    hash = "sha256-5vPCtjUiFSI/Ix5dbGmR3TGQsYIwWAUHMwx8yH6HXac=";
   };
 
   sourceRoot = "source/linux-rust";
   cargoHash = "sha256-Ebqx+UU2tdygvqvDGjBSxbkmPnkR47/yL3sCVWo54CU=";
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    pkg-config
+    makeWrapper
+  ];
 
   buildInputs = [
     libpulseaudio
     dbus
+
+    # Iced-rs dependencies
+    vulkan-loader
+    libGL
+    wayland
+    libxkbcommon
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libX11
   ];
 
+  # Allows Iced access to the libraries it needs.
+  postFixup = ''
+    wrapProgram $out/bin/librepods \
+      --suffix LD_LIBRARY_PATH : ${lib.makeLibraryPath finalAttrs.buildInputs}
+  '';
+
   meta = {
-    description = "";
-    homepage = "";
-    license = lib.licenses.unlicense;
+    description = "AirPods liberated from Apple's ecosystem.";
+    longDescription = ''
+      LibrePods unlocks Apple's exclusive AirPods features on non-Apple devices. Get access to noise control modes, adaptive transparency, ear detection, hearing aid, customized transparency mode, battery status, and more - all the premium features you paid for but Apple locked to their ecosystem.
+    '';
+    homepage = "https://github.com/kavishdevar/librepods/tree/main";
+    downloadPage = "https://github.com/kavishdevar/librepods/commit/${finalAttrs.src.rev}";
+    # downloadPage = "https://github.com/kavishdevar/librepods/releases/tag/${finalAttrs.src.tag}";
+    mainProgram = "librepods";
+    license = lib.licenses.gpl3;
+    # platforms = ;
     maintainers = [ ];
   };
 })
-
-# stdenv.mkDerivation (finalAttrs: {
-#   pname = "librepods";
-#   version = "0.1.0";
-
-#   src = fetchFromGitHub {
-#     owner = "kavishdevar";
-#     repo = "librepods";
-#     tag = "linux-v${finalAttrs.version}";
-#     hash = "sha256-ZvHbSSW0rfcsNUORZURe0oBHQbnqmS5XT9ffVMwjIMU=";
-#   };
-
-#   buildInputs = [
-#     qt6.qtbase
-#     qt6.qtmultimedia
-#     qt6.qtconnectivity
-#   ];
-
-#   nativeBuildInputs = [
-#     qt6.wrapQtAppsHook
-#     cmake
-#     pkg-config
-#     libpulseaudio
-#   ];
-
-#   configurePhase = ''
-#     runHook preConfigure
-
-#     cmake linux/CMakeLists.txt
-
-#     runHook postConfigure
-#   '';
-
-#   installPhase = ''
-#     runHook preInstall
-
-#     mkdir -p $out/bin $out/applications
-#     cp librepods $out/bin
-#     cp linux/assets/me.kavishdevar.librepods.desktop $out/applications/
-
-#     runHook postInstall
-#   '';
-
-#   postInstall = ''
-#     wrapProgram $out/bin/librepods \
-#       --unset QT_STYLE_OVERRIDE
-#   '';
-
-#   meta = {
-#     description = "AirPods liberated from Apple's ecosystem.";
-#     longDescription = ''
-#       LibrePods unlocks Apple's exclusive AirPods features on non-Apple devices. Get access to noise control modes, adaptive transparency, ear detection, hearing aid, customized transparency mode, battery status, and more - all the premium features you paid for but Apple locked to their ecosystem.
-#     '';
-#     homepage = "https://github.com/kavishdevar/librepods/tree/main";
-#     downloadPage = "https://github.com/kavishdevar/librepods/releases/tag/${finalAttrs.src.tag}";
-#     mainProgram = "librepods";
-#     license = lib.licenses.gpl3;
-#     # platforms = ;
-#     maintainers = with lib.maintainers; [ redlonghead ];
-#   };
-
-# })
