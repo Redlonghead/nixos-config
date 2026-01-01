5aca24d32d510cf23e6f694faac14ae22e1131c7
Redlonghead <git@beardit.net>
2026-01-01 13:44:41 -0600

    style: move auto downloaded calibre plugins to their own folder
    when put in the plugins folder they will not be auto installed so its
    better to have a seperate folder and install them.

--- home/beacon/common/optional/uiApps/calibre.nix
+++ home/beacon/common/optional/uiApps/calibre.nix
@@ -7,11 +7,11 @@
 {
   home.packages = [ pkgs.calibre ];
 
-  # auto add DeDRM and ACSM Input plugins for Calibre
+  # auto download DeDRM and ACSM Input plugins for Calibre
   home.file = {
-    ".config/calibre/plugins/ACSM Input.zip".source =
+    ".config/calibre/nixos-plugins/ACSM Input.zip".source =
       inputs.calibrePlugins.packages.x86_64-linux.acsm-calibre-plugin;
-    ".config/calibre/plugins/DeDRM.zip".source =
+    ".config/calibre/nixos-plugins/DeDRM.zip".source =
       inputs.calibrePlugins.packages.x86_64-linux.dedrm-plugin;
   };
 }
